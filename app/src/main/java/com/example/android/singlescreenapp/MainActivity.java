d64ea3122618fd9e5f90bb8356304b090ff1cb3e
DivyaThakur24 <greyblack2406@gmail.com>
2018-04-03 02:18:52 +0530

    First Commit

--- app/src/main/java/com/example/android/singlescreenapp/MainActivity.java
+++ app/src/main/java/com/example/android/singlescreenapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -15,13 +16,15 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        loadFragment(new HomeFragment());
-
         setContentView(R.layout.activity_main);
+
+            //loading the default fragment
+            loadFragment(new HomeFragment());
 
         //getting bottom navigation view and attaching the listener
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
+
     }
 
     private boolean loadFragment(android.support.v4.app.Fragment fragment){
